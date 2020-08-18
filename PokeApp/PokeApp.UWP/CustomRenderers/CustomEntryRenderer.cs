4edc76b8df9697ffcf390f70823f8f7e2fb28b9e
Steven Atkinson <mrstebouk@gmail.com>
2020-08-18 19:40:41 +0100

    Added a custom renderer for the Entry component on UWP

---
+++ PokeApp/PokeApp.UWP/CustomRenderers/CustomEntryRenderer.cs
@@ -0,0 +1,21 @@
+﻿using PokeApp.CustomControls;
+using PokeApp.UWP.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace PokeApp.UWP.CustomRenderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null) return;
+
+            Control.Background = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.LightGreen);
+            Control.BorderThickness = new Windows.UI.Xaml.Thickness(0, 0, 0, 2);
+        }
+    }
+}
