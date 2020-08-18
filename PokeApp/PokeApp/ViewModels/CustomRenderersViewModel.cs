548fabd1379d4ad8ae524a46769ce903797fdc7e
Steven Atkinson <mrstebouk@gmail.com>
2020-08-18 09:46:47 +0100

    Started work on showing a custom Entry control

---
+++ PokeApp/PokeApp/ViewModels/CustomRenderersViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+using PokeApp.Views.CustomRenderers;
+using Xamarin.Forms;
+
+namespace PokeApp.ViewModels
+{
+    public class CustomRenderersViewModel
+    {
+        public CustomRenderersViewModel()
+        {
+            GoToEntryCustomRenderersPage = new Command(ExecuteGoToEntryCustomRenderersPage);
+        }
+
+        public ICommand GoToEntryCustomRenderersPage { get; }
+        public INavigation Navigation { get; set; }
+
+        private async void ExecuteGoToEntryCustomRenderersPage(object obj)
+        {
+            await Navigation.PushAsync(new EntryCustomRendererPage());
+        }
+    }
+}
---
+++ PokeApp/PokeApp/Views/CustomRenderers/EntryCustomRendererPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PokeApp.Views.CustomRenderers
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryCustomRendererPage : ContentPage
+    {
+        public EntryCustomRendererPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
