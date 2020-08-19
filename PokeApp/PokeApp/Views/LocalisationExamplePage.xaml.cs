dbc3dae0e550adfa940bd13530c728a2f9421e21
Steven Atkinson <mrstebouk@gmail.com>
2020-08-19 12:39:26 +0100

    Started working on the localisation example page

---
+++ PokeApp/PokeApp/Views/LocalisationExamplePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PokeApp.Views
+{
+    public partial class LocalisationExamplePage : ContentPage
+    {
+        public LocalisationExamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
