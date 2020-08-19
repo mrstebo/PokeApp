e6c6b7aa0b237cc83fb115aa364747d493542c6b
Steven Atkinson <mrstebouk@gmail.com>
2020-08-19 10:28:19 +0100

    Added an example for a horizontal collection view

--- PokeApp/PokeApp/ViewModels/CollectionViewExampleViewModel.cs
+++ PokeApp/PokeApp/ViewModels/CollectionViewExampleViewModel.cs
@@ -9,16 +9,23 @@ namespace PokeApp.ViewModels
         public CollectionViewExampleViewModel()
         {
             GoToCollectionViewGridPage = new Command(ExecuteGoToCollectionViewGridPage);
+            GoToCollectionViewLinearHorizontalPage = new Command(ExecuteGoToCollectionViewLinearHorizontalPage);
             GoToCollectionViewLinearVerticalPage = new Command(ExecuteGoToCollectionViewLinearVerticalPage);
         }
 
         public ICommand GoToCollectionViewGridPage { get; }
+        public ICommand GoToCollectionViewLinearHorizontalPage { get; }
         public ICommand GoToCollectionViewLinearVerticalPage { get; }
         public INavigation Navigation { get; set; }
 
         private async void ExecuteGoToCollectionViewGridPage()
         {
             await Navigation.PushAsync(new CollectionViewGridPage());
+        }
+
+        private async void ExecuteGoToCollectionViewLinearHorizontalPage()
+        {
+            await Navigation.PushAsync(new CollectionViewLinearHorizontalPage());
         }
 
         private async void ExecuteGoToCollectionViewLinearVerticalPage()
---
+++ PokeApp/PokeApp/Views/CollectionViews/CollectionViewLinearHorizontalPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PokeApp.ViewModels.CollectionViews;
+using Xamarin.Forms;
+
+namespace PokeApp.Views.CollectionViews
+{
+    public partial class CollectionViewLinearHorizontalPage : ContentPage
+    {
+        private readonly CollectionViewViewModel viewModel;
+
+        public CollectionViewLinearHorizontalPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new CollectionViewViewModel();
+        }
+
+        void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            viewModel.LoadData.Execute(null);
+        }
+    }
+}
